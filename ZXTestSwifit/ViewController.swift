17ecae4022964564bd1301ea3d0e2076be19f53c
zhangxin0915 <macmini@ZW.local>
2015-12-09 09:36:25 +0800

    学习Swift常量

---
+++ ZXTestSwifit/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ZXTestSwifit
+//
+//  Created by macmini on 15/12/9.
+//  Copyright © 2015年 macmini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 常量
+        let a = 3;
+        let tempString: String = "swift";
+        print(a);
+        print(tempString);
+        print(a,tempString);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
